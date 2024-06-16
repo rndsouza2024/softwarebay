17ad30182fa5e51bccde807e9a3295c247fbe978
mxplayermovies <dsouzarnd@gmail.com>
2024-06-16 09:42:16 +0530

    clear

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://softwarebay.vercel.app/',
-    changefreq: 'daily',
-    priority: 0.90,
-    sitemapSize: 7000,
-  }
